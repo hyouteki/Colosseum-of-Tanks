a7b8e670f76545f211289a5e8976fdadf4d95fb8
s0t-chit-0n0nd <108231924+s0t-chit-0n0nd@users.noreply.github.com>
2022-11-19 23:08:02 +0530

    Delete Colosseum of Tanks directory

--- Colosseum of Tanks/core/src/com/mygdx/colosseum_of_tanks/classes/Game.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.colosseum_of_tanks.classes;
-
-public abstract class Game {
-    public void start() {}
-    public void make_drop() {}
-    public void end() {}
-
-    public abstract void create();
-}
